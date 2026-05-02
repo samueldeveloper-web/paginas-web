e3c104d75257f610d74968e6b560affc994f1821
samuel <samgonzalezt23@gmail.com>
2026-05-02 16:39:10 -0500

    Add Supabase auth login configuration

---
+++ app-config.example.js
@@ -0,0 +1,4 @@
+window.TREX_CONFIG = {
+  supabaseUrl: "https://TU-PROYECTO.supabase.co",
+  supabaseAnonKey: "TU_SUPABASE_ANON_KEY",
+};
---
+++ app-config.js
@@ -0,0 +1,4 @@
+window.TREX_CONFIG = {
+  supabaseUrl: "https://ekrladnjnsixdutsdlrd.supabase.co",
+  supabaseAnonKey: "sb_publishable_gcH7V92KH0ajIHAomSzlBg_kDhLQXVr",
+};
